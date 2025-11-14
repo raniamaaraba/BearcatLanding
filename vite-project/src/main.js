1ccf90db237af2150d152e63edc1ddc2ed08b769
raniamaaraba <145875065+raniamaaraba@users.noreply.github.com>
2025-11-14 12:28:32 -0500

    Tiny deployment fixes

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,10 +1,11 @@
-import { mount } from 'svelte'
-import './app.css'
-import App from './App.svelte'
+import App from './App.svelte';
 import './app.css';
 
+const app = new App({
+  target: document.getElementById('app')
+});
 
-mount(App, { target: document.getElementById('app') });
-export default App;
+export default app;
+
 
 
--- vite-project/svelte.config.js
+++ vite-project/svelte.config.js
@@ -1,7 +1,10 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+  compilerOptions: {
+    compatibility: {
+      componentApi: 4
+    }
+  },
+  preprocess: vitePreprocess()
+};
